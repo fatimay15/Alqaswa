bc35f501128dad75f6d6e4db9344235053255ecf
fatimay15 <fatimamughal273@gmail.com>
2023-07-22 01:55:24 +0500

    my footer

--- footer.php
+++ footer.php
@@ -28,12 +28,12 @@
         <p>
         Unleashing potential through advanced polymer technology. Experience excellence with Alqaswa Polymers. Empowering industries, driving innovation, and delivering sustainable solutions.
         </p>
-        <div class="social-links mt-3">
+        <!-- <div class="social-links mt-3">
           <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
           <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
           <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
           <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-        </div>
+        </div> -->
       </div>
 
       <div class="col-lg-2 col-6 footer-links">
@@ -81,6 +81,7 @@
     &copy; Copyright <strong><span>Alqaswa Polymers</span></strong>. All Rights Reserved
   </div>
   <div class="credits">
+    Powered by <a href="https://stlllr.com/"><strong>StlllrR</strong><a> 
     <!-- All the links in the footer should remain intact. -->
     <!-- You can delete the links only if you purchased the pro version. -->
     <!-- Licensing information: https://bootstrapmade.com/license/ -->
